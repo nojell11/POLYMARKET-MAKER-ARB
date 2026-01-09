950d4ca46cdeb84c6d8efca904bc3a6ec59876b2
nojell11 <159235569+nojell11@users.noreply.github.com>
2026-01-10 00:30:36 +0700

    Update license.js

--- src/license.js
+++ src/license.js
@@ -4,7 +4,7 @@
 import crypto from 'crypto';
 
 // Secret salt - CHANGE THIS to your own secret!
-const SECRET = 'const SECRET = 'polymarket-arb-bot-2026-secret-key';
+const SECRET = 'polymarket-arb-bot-2026-secret-key';
 
 /**
  * Generate a license key for a given expiry date
